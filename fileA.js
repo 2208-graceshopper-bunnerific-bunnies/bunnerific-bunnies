eb3b755f043535e401db43c9dd04f67f0f04c449
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2022-10-03 08:46:19 -0700

    Jon is better

--- fileA.js
+++ fileA.js
@@ -1,3 +1,15 @@
 console.log('hello');
 console.log('Alfred!');
 
+console.log('Jon is better!');
+console.log('Jon is better!');
+console.log('Jon is better!');
+console.log('Jon is better!');
+console.log('Jon is better!');
+console.log('Jon is better!');
+console.log('Jon is better!');
+console.log('Jon is better!');
+console.log('Jon is better!');
+console.log('Jon is better!');
+console.log('Jon is better!');
+console.log('Jon is better!');
