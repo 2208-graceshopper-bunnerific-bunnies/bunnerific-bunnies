c9aaafb07f761e64278bf763fd0d8becd2c94b60
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2022-10-03 08:44:56 -0700

    Alfred forever

--- fileA.js
+++ fileA.js
@@ -1,3 +1,26 @@
 console.log('hello');
 console.log('Alfred!');
 
+
+
+
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
+console.log('Alfred!');
